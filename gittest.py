1960286045f0f0787592fe5ec0c47c05f6d89306
Student E1 <e1@akmith-edu.local>
2025-11-11 20:14:33 +0200

    PAOK

---
+++ gittest.py
@@ -0,0 +1,23 @@
+sum=0
+j=0
+while sum<5:
+    passw = input ("dwse kodiko: ")
+    for x in passw:
+        j+=1
+    if j>=6 and j<=16:
+        sum+=1
+    for x in passw:
+        if x in ("qwertyuiopasdfghjklzxcvbnm"):
+            sum+=1
+            
+        if x in ("QWERTYUIOPASDFGHJKLZXCVBNM"):
+            sum+=1
+            
+        if x in ("1234567890"):
+            sum+=1
+           
+        if x in ("$#@"):
+            sum+=1
+            
+        
+print("swstos kodikos")
